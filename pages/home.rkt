b0ffdfb18041dfda5990697dec1a6eaff4526f54
Christopher Vote <cbourjaily@gmail.com>
2026-08-16 02:11:56 +0800

    temporary text modification

--- pages/home.rkt
+++ pages/home.rkt
@@ -14,6 +14,10 @@
       "Here you'll find a collection of software projects I have built.")
      
      (p
-      "Feel free to explore using the navigation above."))))
+      "Feel free to explore using the navigation above.")
+      
+     (p
+      "Impact-Site-Verification: 1135a262-4059-49c1-9d13-04ed90521cdf"))))
+      
 
 (provide render-home-page)
