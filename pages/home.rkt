65a09a20e7d3372cb51bb82a706b5c8d6074c25b
Christopher Vote <cbourjaily@gmail.com>
2026-08-16 02:14:38 +0800

    text modification reverted

--- pages/home.rkt
+++ pages/home.rkt
@@ -14,10 +14,7 @@
       "Here you'll find a collection of software projects I have built.")
      
      (p
-      "Feel free to explore using the navigation above.")
-      
-     (p
-      "Impact-Site-Verification: 1135a262-4059-49c1-9d13-04ed90521cdf"))))
+      "Feel free to explore using the navigation above."))))
       
 
 (provide render-home-page)
